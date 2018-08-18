1eb757d426137599303437964688dc70e52600cf
shellscape <andrew@shellscape.org>
2018-08-17 22:46:23 -0400

    refactor: make benchmarks a bin, add style

--- bin/bench.js
+++ bin/bench.js
@@ -1,17 +1,47 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
+const { createReadStream } = require('fs')
+const { basename } = require('path')
+const globby = require('globby')
+const chalk = require('chalk')
+const table = require('text-table')
+const strip = require('strip-ansi')
+
 const csv = require('../')
 
-const now = Date.now()
-const [,, fileName] = process.argv
-let rows = 0
+const run = async () => {
+  const paths = await globby(['test/data/*.csv'])
+  const rows = []
+  let rowsParsed = 0
 
-fs.createReadStream(fileName || '/tmp/tmp.csv')
-  .pipe(csv())
-  .on('data', (line) => {
-    rows++
+  for (const path of paths) {
+    await new Promise((resolve) => {
+      const now = Date.now()
+      createReadStream(path)
+        .pipe(csv())
+        .on('data', (line) => {
+          rowsParsed++
+        })
+        .on('end', () => {
+          const duration = Date.now() - now
+          const color = duration <= 10 ? 'green' : (duration > 100 ? 'red' : 'yellow')
+          const fileName = chalk.blue(basename(path))
+          rows.push(['', fileName, rowsParsed, chalk[color](`${duration}ms`)])
+          resolve()
+        })
+    })
+  }
+
+  rows.unshift(['', 'Filename', 'Rows Parsed', 'Duration'].map(h => chalk.dim.underline(h)))
+
+  const results = table(rows, {
+    align: [ 'l', 'l', 'r', 'r' ],
+    stringLength (str) {
+      return strip(str).length
+    }
   })
-  .on('end', () => {
-    console.log(`${fileName}: parsed ${rows} rows in ${Date.now() - now} ms`)
-  })
+
+  console.log(`\n${results}`)
+}
+
+run()
