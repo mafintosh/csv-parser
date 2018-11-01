0fcb641fe0e5c80e5263124c08cfce2ce4143be8
Fabien Rogeret <fabien.rogeret@gmail.com>
2018-11-01 09:48:35 -0400

    chore: Add Typescript definition (#104)
    * Add Typescript definition
    
    * Update index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+declare module 'csv-parser' {
+  import { Transform } from 'stream';
+
+  interface CsvParser extends Transform {}
+  interface CsvParserOptions {
+    escape?: string;
+    headers?: ReadonlyArray<string> | boolean;
+    mapHeaders?: (args: { header: string; index: number }) => string | null;
+    mapValues?: (args: { header: string; index: number; value: any }) => any;
+    newline?: string;
+    quote?: string;
+    raw?: boolean;
+    separator?: string;
+    skipLines?: number;
+    maxRowBytes?: number;
+    strict?: boolean;
+  }
+
+  const csvParser: (
+    optionsOrHeaders?: CsvParserOptions | ReadonlyArray<string>
+  ) => CsvParser;
+
+  export = csvParser;
+}
