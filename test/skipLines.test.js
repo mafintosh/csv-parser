55aac1fb55caceec4a35a0504b0e6a10c182be2f
shellscape <andrew@shellscape.org>
2019-07-05 10:42:21 -0400

    chore: fix linting errors

--- test/skipLines.test.js
+++ test/skipLines.test.js
@@ -6,21 +6,21 @@ test.cb('skip lines', (t) => {
   const verify = (err, lines) => {
     t.false(err, 'no err')
     t.is(lines.length, 1, '1 row')
-    t.is(JSON.stringify(lines[0]), JSON.stringify({yes: 'ok', yup: 'ok', yeah: 'ok!'}))
+    t.is(JSON.stringify(lines[0]), JSON.stringify({ yes: 'ok', yup: 'ok', yeah: 'ok!' }))
     t.end()
   }
 
-  collect('junk_rows.csv', {skipLines: 2}, verify)
+  collect('junk_rows.csv', { skipLines: 2 }, verify)
 })
 
 test.cb('skip lines with headers', (t) => {
   const verify = (err, lines) => {
     t.false(err, 'no err')
     t.is(lines.length, 2, '2 rows')
-    t.is(JSON.stringify(lines[0]), JSON.stringify({s: 'yes', p: 'yup', h: 'yeah'}))
-    t.is(JSON.stringify(lines[1]), JSON.stringify({s: 'ok', p: 'ok', h: 'ok!'}))
+    t.is(JSON.stringify(lines[0]), JSON.stringify({ s: 'yes', p: 'yup', h: 'yeah' }))
+    t.is(JSON.stringify(lines[1]), JSON.stringify({ s: 'ok', p: 'ok', h: 'ok!' }))
     t.end()
   }
 
-  collect('junk_rows.csv', {headers: ['s', 'p', 'h'], skipLines: 2}, verify)
+  collect('junk_rows.csv', { headers: ['s', 'p', 'h'], skipLines: 2 }, verify)
 })
