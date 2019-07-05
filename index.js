66ac1b7c429dfa3e9015fb3bbb5eda3380363b1d
shellscape <andrew@shellscape.org>
2019-07-05 16:37:56 -0400

    refactor: move state props to this.state, use this.options

--- index.js
+++ index.js
@@ -29,40 +29,157 @@ class CsvParser extends Transform {
 
     const options = Object.assign({}, defaults, opts)
 
-    this.customNewline = options.newline !== defaults.newline
+    options.customNewline = options.newline !== defaults.newline
 
-    for (const key of Object.keys(options)) {
-      if (['newline', 'quote', 'separator'].includes(key)) {
+    for (const key of ['newline', 'quote', 'separator']) {
+      if (typeof options[key] !== 'undefined') {
         ([options[key]] = bufferFrom(options[key]))
       }
-      // legacy codebase support
-      this[key] = options[key]
     }
 
     // if escape is not defined on the passed options, use the end value of quote
-    this.escape = (opts || {}).escape ? bufferFrom(options.escape)[0] : options.quote
+    options.escape = (opts || {}).escape ? bufferFrom(options.escape)[0] : options.quote
 
-    this._prev = null
-    this._prevEnd = 0
-    this._first = true
-    this._quoted = false
-    this._escaped = false
-    this._empty = this._raw ? bufferAlloc(0) : ''
-    this._Row = null
-    this._currentRowBytes = 0
-    this._line = 0
-
-    if (this.headers === false) {
-      // enforce, as the column length check will fail if headers:false
-      this.strict = false
+    this.state = {
+      empty: options.raw ? bufferAlloc(0) : '',
+      escaped: false,
+      first: true,
+      lineNumber: 0,
+      previousEnd: 0,
+      rowLength: 0,
+      quoted: false
     }
 
-    if (this.headers || this.headers === false) {
-      this._first = false
+    this._prev = null
+
+    if (options.headers === false) {
+      // enforce, as the column length check will fail if headers:false
+      options.strict = false
+    }
+
+    if (options.headers || options.headers === false) {
+      this.state.first = false
+    }
+
+    this.options = options
+    this.headers = options.headers
+  }
+
+  parseCell (buffer, start, end) {
+    const { escape, quote } = this.options
+    // remove quotes from quoted cells
+    if (buffer[start] === quote && buffer[end - 1] === quote) {
+      start++
+      end--
+    }
+
+    let y = start
+
+    for (let i = start; i < end; i++) {
+      // check for escape characters and skip them
+      if (buffer[i] === escape && i + 1 < end && buffer[i + 1] === quote) {
+        i++
+      }
+
+      if (y !== i) {
+        buffer[y] = buffer[i]
+      }
+      y++
+    }
+
+    return this.parseValue(buffer, start, y)
+  }
+
+  parseLine (buffer, start, end) {
+    const { customNewline, escape, mapHeaders, mapValues, quote, separator, skipComments, skipLines } = this.options
+
+    end-- // trim newline
+    if (!customNewline && buffer.length && buffer[end - 1] === cr) {
+      end--
+    }
+
+    const comma = separator
+    const cells = []
+    let isQuoted = false
+    let offset = start
+
+    if (skipComments) {
+      const char = typeof skipComments === 'string' ? skipComments : '#'
+      if (buffer[start] === bufferFrom(char)[0]) {
+        return
+      }
+    }
+
+    const mapValue = (value) => {
+      if (this.state.first) {
+        return value
+      }
+
+      const index = cells.length
+      const header = this.headers[index]
+
+      return mapValues({ header, index, value })
+    }
+
+    for (let i = start; i < end; i++) {
+      const isStartingQuote = !isQuoted && buffer[i] === quote
+      const isEndingQuote = isQuoted && buffer[i] === quote && i + 1 <= end && buffer[i + 1] === comma
+      const isEscape = isQuoted && buffer[i] === escape && i + 1 < end && buffer[i + 1] === quote
+
+      if (isStartingQuote || isEndingQuote) {
+        isQuoted = !isQuoted
+        continue
+      } else if (isEscape) {
+        i++
+        continue
+      }
+
+      if (buffer[i] === comma && !isQuoted) {
+        let value = this.parseCell(buffer, offset, i)
+        value = mapValue(value)
+        cells.push(value)
+        offset = i + 1
+      }
+    }
+
+    if (offset < end) {
+      let value = this.parseCell(buffer, offset, end)
+      value = mapValue(value)
+      cells.push(value)
+    }
+
+    if (buffer[end - 1] === comma) {
+      cells.push(mapValue(this.state.empty))
+    }
+
+    const skip = skipLines && skipLines > this.state.lineNumber
+    this.state.lineNumber++
+
+    if (this.state.first && !skip) {
+      this.state.first = false
+      this.headers = cells.map((header, index) => mapHeaders({ header, index }))
+
+      this.emit('headers', this.headers)
+      return
+    }
+
+    if (this.options.strict && cells.length !== this.headers.length) {
+      const e = new RangeError('Row length does not match headers')
+      this.emit('error', e)
+    } else {
+      if (!skip) this.writeRow(cells)
     }
   }
 
-  _emit (cells) {
+  parseValue (buffer, start, end) {
+    if (this.options.raw) {
+      return buffer.slice(start, end)
+    }
+
+    return buffer.toString('utf-8', start, end)
+  }
+
+  writeRow (cells) {
     if (this.headers === false || cells.length > this.headers.length) {
       this.headers = cells.map((value, index) => index)
     }
@@ -79,181 +196,81 @@ class CsvParser extends Transform {
   }
 
   _flush (cb) {
-    if (this._escaped || !this._prev) return cb()
-    this._online(this._prev, this._prevEnd, this._prev.length + 1) // plus since online -1s
+    if (this.state.escaped || !this._prev) return cb()
+    this.parseLine(this._prev, this.state.previousEnd, this._prev.length + 1) // plus since online -1s
     cb()
   }
 
-  _oncell (buf, start, end) {
-    // remove quotes from quoted cells
-    if (buf[start] === this.quote && buf[end - 1] === this.quote) {
-      start++
-      end--
-    }
-
-    let y = start
-
-    for (let i = start; i < end; i++) {
-      // check for escape characters and skip them
-      if (buf[i] === this.escape && i + 1 < end && buf[i + 1] === this.quote) i++
-      if (y !== i) buf[y] = buf[i]
-      y++
-    }
-
-    const value = this._onvalue(buf, start, y)
-    return value
-  }
-
-  _online (buf, start, end) {
-    end-- // trim newline
-    if (!this.customNewline && buf.length && buf[end - 1] === cr) end--
-
-    const comma = this.separator
-    const cells = []
-    let isQuoted = false
-    let offset = start
-
-    const { skipComments } = this
-    if (skipComments) {
-      const char = typeof skipComments === 'string' ? skipComments : '#'
-      if (buf[start] === bufferFrom(char)[0]) {
-        return
-      }
-    }
-
-    const mapValue = (value) => {
-      if (this._first) {
-        return value
-      }
-
-      const index = cells.length
-      const header = this.headers[index]
-
-      return this.mapValues({ header, index, value })
-    }
-
-    for (let i = start; i < end; i++) {
-      const isStartingQuote = !isQuoted && buf[i] === this.quote
-      const isEndingQuote = isQuoted && buf[i] === this.quote && i + 1 <= end && buf[i + 1] === comma
-      const isEscape = isQuoted && buf[i] === this.escape && i + 1 < end && buf[i + 1] === this.quote
-
-      if (isStartingQuote || isEndingQuote) {
-        isQuoted = !isQuoted
-        continue
-      } else if (isEscape) {
-        i++
-        continue
-      }
-
-      if (buf[i] === comma && !isQuoted) {
-        let value = this._oncell(buf, offset, i)
-        value = mapValue(value)
-        cells.push(value)
-        offset = i + 1
-      }
-    }
-
-    if (offset < end) {
-      let value = this._oncell(buf, offset, end)
-      value = mapValue(value)
-      cells.push(value)
-    }
-
-    if (buf[end - 1] === comma) {
-      cells.push(mapValue(this._empty))
-    }
-
-    const skip = this.skipLines && this.skipLines > this._line
-    this._line++
-
-    if (this._first && !skip) {
-      this._first = false
-      this.headers = cells.map((header, index) => this.mapHeaders({ header, index }))
-
-      this.emit('headers', this.headers)
-      return
-    }
-
-    if (this.strict && cells.length !== this.headers.length) {
-      const e = new RangeError('Row length does not match headers')
-      this.emit('error', e)
-    } else {
-      if (!skip) this._emit(cells)
-    }
-  }
-
-  _onvalue (buf, start, end) {
-    if (this._raw) return buf.slice(start, end)
-    return buf.toString('utf-8', start, end)
-  }
-
   _transform (data, enc, cb) {
-    if (typeof data === 'string') data = bufferFrom(data)
+    if (typeof data === 'string') {
+      data = bufferFrom(data)
+    }
 
+    const { escape, quote } = this.options
     let start = 0
-    let buf = data
+    let buffer = data
 
     if (this._prev) {
       start = this._prev.length
-      buf = Buffer.concat([this._prev, data])
+      buffer = Buffer.concat([this._prev, data])
       this._prev = null
     }
 
-    const bufLen = buf.length
+    const bufferLength = buffer.length
 
-    for (let i = start; i < bufLen; i++) {
-      const chr = buf[i]
-      const nextChr = i + 1 < bufLen ? buf[i + 1] : null
+    for (let i = start; i < bufferLength; i++) {
+      const chr = buffer[i]
+      const nextChr = i + 1 < bufferLength ? buffer[i + 1] : null
 
-      this._currentRowBytes++
-      if (this._currentRowBytes > this.maxRowBytes) {
+      this.state.rowLength++
+      if (this.state.rowLength > this.options.maxRowBytes) {
         return cb(new Error('Row exceeds the maximum size'))
       }
 
-      if (!this._escaped && chr === this.escape && nextChr === this.quote && i !== start) {
-        this._escaped = true
+      if (!this.state.escaped && chr === escape && nextChr === quote && i !== start) {
+        this.state.escaped = true
         continue
-      } else if (chr === this.quote) {
-        if (this._escaped) {
-          this._escaped = false
+      } else if (chr === quote) {
+        if (this.state.escaped) {
+          this.state.escaped = false
           // non-escaped quote (quoting the cell)
         } else {
-          this._quoted = !this._quoted
+          this.state.quoted = !this.state.quoted
         }
         continue
       }
 
-      if (!this._quoted) {
-        if (this._first && !this.customNewline) {
+      if (!this.state.quoted) {
+        if (this.state.first && !this.options.customNewline) {
           if (chr === nl) {
-            this.newline = nl
+            this.options.newline = nl
           } else if (chr === cr) {
             if (nextChr !== nl) {
-              this.newline = cr
+              this.options.newline = cr
             }
           }
         }
 
-        if (chr === this.newline) {
-          this._online(buf, this._prevEnd, i + 1)
-          this._prevEnd = i + 1
-          this._currentRowBytes = 0
+        if (chr === this.options.newline) {
+          this.parseLine(buffer, this.state.previousEnd, i + 1)
+          this.state.previousEnd = i + 1
+          this.state.rowLength = 0
         }
       }
     }
 
-    if (this._prevEnd === bufLen) {
-      this._prevEnd = 0
+    if (this.state.previousEnd === bufferLength) {
+      this.state.previousEnd = 0
       return cb()
     }
 
-    if (bufLen - this._prevEnd < data.length) {
+    if (bufferLength - this.state.previousEnd < data.length) {
       this._prev = data
-      this._prevEnd -= (bufLen - data.length)
+      this.state.previousEnd -= (bufferLength - data.length)
       return cb()
     }
 
-    this._prev = buf
+    this._prev = buffer
     cb()
   }
 }
